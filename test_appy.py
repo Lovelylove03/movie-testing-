e47a89593d0168c6114ae973d6aaf866eb45d417
Lavender Oyugi <Lavenderoyugi1@gmail.com>
2024-08-07 16:45:56 +0200

    Update test_appy.py

--- test_appy.py
+++ test_appy.py
@@ -5,7 +5,8 @@ from sklearn.neighbors import NearestNeighbors
 # st.set_page_config(layout='wide')
 
 url = 'https://image.tmdb.org/t/p/original'
-df= pd.read_csv(df_ml - df_ml.csv)
+link = https://raw.githubusercontent.com/Lovelylove03/movie-testing-/main/df_ml%20-%20df_ml.csv
+df= pd.read_csv(link)
 st.title('Système de recommandations de films')
 st.divider()
 
